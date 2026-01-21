cff440371bfe7baf318365bc81bdec9b2d0d87fe
Nicolas_dmb <gyfeix@gmail.com>
2026-01-21 08:30:44 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-start justify-between p-8 bg-pink-100">
+      <h1 className="text-xl font-bold underline">
+        Hello Tailwind 👋
+      </h1>
+    </div>
+  )
+}
+
+export default App
