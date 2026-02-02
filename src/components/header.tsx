8c24d8f5d41dccd24ada86cb87c5f0d8f55dc0a9
Nicolas_dmb <gyfeix@gmail.com>
2026-02-02 08:39:55 +0100

    feat : add more button for smartphone and touchpad

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,9 @@
 import useDevice from "../hook/useDevice.tsx";
 import useScrollDirection from "../hook/useScroll.tsx";
 import useHeader from "../hook/useHeader.tsx";
+import more from "/public/more.svg";
+import close from "/public/close.svg";
+import { useState } from "react";
 
 const buttonStyle =
   "text-[18px] transform transition-transform duration-300 hover:-translate-y-0.5 hover:scale-110 cursor-pointer text-numa-black font-cormorant font-bold";
@@ -12,6 +15,7 @@ export default function Header() {
   const { islaptop, istouchpad } = useDevice();
   const scrollDirection = useScrollDirection();
   const { displayServices, open, close, setDisplayServices } = useHeader({ scrollDirection });
+  const [seeMore, setSeeMore] = useState(false);
 
   return (
     <>
@@ -27,35 +31,18 @@ export default function Header() {
             onForceClose={() => setDisplayServices(false)}
           />
         )}
-        {istouchpad && <TouchpadHeader />}
-        {!islaptop && !istouchpad && <MobileHeader />}
+        {istouchpad && <TouchpadHeader setSeeMore={setSeeMore} />}
+        {!islaptop && !istouchpad && <MobileHeader setSeeMore={setSeeMore} />}
       </section>
 
       {islaptop && (
-        <div
-          className={`fixed left-0 w-full z-40 px-8 ${
-            scrollDirection === "down" ? "-translate-y-full hidden" : "translate-y-0"
-          } transition-transform duration-300`}
-          style={{ top: "64px" }}
-        >
-          <div
-            onMouseLeave={close}
-            className={`
-              mx-auto w-72
-              bg-[#ebe6e277] backdrop-blur-sm shadow-lg
-              rounded-b-lg
-              border-t border-black/10
-              overflow-hidden
-              transition-all duration-200
-              ${displayServices ? "opacity-100 translate-y-0 pointer-events-auto" : "opacity-0 -translate-y-1 pointer-events-none"}
-            `}
-          >
-            <a className={subButtonStyle + " block px-5 py-3"} href="/#services-voyages">MES SERVICES VOYAGES</a>
-            <a className={subButtonStyle + " block px-5 py-3"} href="/#travel-planner">TRAVEL PLANNER</a>
-            <a className={subButtonStyle + " block px-5 py-3"} href="/#accompagnement">ACCOMPAGNEMENT</a>
-          </div>
-        </div>
+        <LaptopServices
+          displayServices={displayServices}
+          close={close}
+          scrollDirection={scrollDirection}
+        />
       )}
+      {!islaptop && (<MoreSections isTouchpad={istouchpad} setSeeMore={setSeeMore} seeMore={seeMore} scrollDirection={scrollDirection} />)}
     </>
   );
 }
@@ -86,7 +73,12 @@ function LaptopHeader({ onOpenServices, onForceClose }: LaptopHeaderProps) {
   );
 }
 
-function TouchpadHeader() {
+interface MdHeaderProps {
+  setSeeMore: (seeMore: boolean) => void;
+}
+
+function TouchpadHeader({ setSeeMore }: MdHeaderProps) {
+
   return <div
       className="text-numa-black flex items-center justify-between h-16 gap-8 items-center px-4"
     >
@@ -96,22 +88,83 @@ function TouchpadHeader() {
         <a className="text-[22px] text-numa-red font-cormorant font-semibold">
             CONTACT
         </a>
+        <img src={more} alt="More options" className="w-6 h-6 cursor-pointer" onClick={() => setSeeMore(true)} />
       </div>
     </div>
 }
 
-function MobileHeader() {
+function MobileHeader({ setSeeMore }: MdHeaderProps) {
   return(
     <div
       className="text-numa-black flex items-center justify-between h-16 gap-8 items-center"
     >
       <a className="text-[14px] font-cormorant font-semibold" href="/#first-layer">NUMA TRAVEL</a>
-      <div className="flex gap-6 items-center">
+      <div className="flex gap-4 items-center">
         <a className="text-[14px] font-cormorant font-semibold">EXPLORER</a>
         <a className="text-[14px] font-cormorant font-semibold text-numa-red">
             CONTACT
         </a>
+        <img src={more} alt="More options" className="w-5 h-5 cursor-pointer" onClick={() => setSeeMore(true)} />
       </div>
     </div>
   );
+}
+
+
+interface MoreSectionsProps {
+  isTouchpad: boolean;
+  setSeeMore: (seeMore: boolean) => void;
+  seeMore?: boolean;
+  scrollDirection?: string;
+}
+
+function MoreSections({ isTouchpad, setSeeMore, seeMore, scrollDirection }: MoreSectionsProps){
+  const touchPadStyle = "text-[22px] font-cormorant font-semibold";
+  const mobileStyle = "text-[14px] font-cormorant font-semibold";
+  return (
+    <div className={`fixed top-0 left-0 w-full bg-[#ebe6e277] z-50 backdrop-blur-sm shadow-lg pb-8 px-8 ${seeMore ? "block" : "hidden"} transition-transform duration-300 ${scrollDirection === "down" ? "-translate-y-full" : "translate-y-0"}`}>
+      <img src={close} alt="Close options" className={`cursor-pointer float-right mt-4 ${isTouchpad ? "w-5 h-5" : "w-5 h-5"}`} onClick={() => setSeeMore(false)} />
+      <div className="flex flex-col gap-4 pt-16 items-center">
+        <a className={isTouchpad ? touchPadStyle : mobileStyle} href="/#services-voyages">QUI SUIS-JE ?</a>
+        <a className={isTouchpad ? touchPadStyle : mobileStyle} href="/#travel-planner">MES SERVICES VOYAGES</a>
+        <a className={isTouchpad ? touchPadStyle : mobileStyle} href="/#travel-planner">TRAVEL PLANNER</a>
+        <a className={isTouchpad ? touchPadStyle : mobileStyle} href="/#accompagnement">ACCOMPAGNEMENT</a>
+      </div>
+    </div>
+  )
+}
+
+
+interface LaptopServicesProps {
+  displayServices: boolean;
+  close: () => void;
+  scrollDirection: string;
+}
+
+function LaptopServices({ displayServices, close, scrollDirection }: LaptopServicesProps){
+  return(
+    <div
+      className={`fixed left-0 w-full z-40 px-8 ${
+        scrollDirection === "down" ? "-translate-y-full hidden" : "translate-y-0"
+      } transition-transform duration-300`}
+      style={{ top: "64px" }}
+    >
+      <div
+        onMouseLeave={close}
+        className={`
+          mx-auto w-72
+          bg-[#ebe6e277] backdrop-blur-sm shadow-lg
+          rounded-b-lg
+          border-t border-black/10
+          overflow-hidden
+          transition-all duration-200
+          ${displayServices ? "opacity-100 translate-y-0 pointer-events-auto" : "opacity-0 -translate-y-1 pointer-events-none"}
+        `}
+      >
+        <a className={subButtonStyle + " block px-5 py-3"} href="/#services-voyages">MES SERVICES VOYAGES</a>
+        <a className={subButtonStyle + " block px-5 py-3"} href="/#travel-planner">TRAVEL PLANNER</a>
+        <a className={subButtonStyle + " block px-5 py-3"} href="/#accompagnement">ACCOMPAGNEMENT</a>
+      </div>
+    </div>
+  )
 }
