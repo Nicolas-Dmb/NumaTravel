ffc9e08b9c8a85f3469141c6955eeecbdc381c65
Nicolas_dmb <gyfeix@gmail.com>
2026-04-25 22:17:05 +0200

    fix: re-enable sendForm functionality by restoring fetch logic and error handling

--- src/features/meta/repositories/sendForm.tsx
+++ src/features/meta/repositories/sendForm.tsx
@@ -2,19 +2,16 @@ import FormResponse from "../model/formResponse";
 
 export default async function sendForm(formResponse: FormResponse, metaEventId: string|undefined, fbp: string|undefined, fbc: string|undefined): Promise<{ status: number; message: string }> {
     
-    // const response = await fetch(`${import.meta.env.VITE_API_URL}/submit_form`, {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify(formResponse.toJson(metaEventId, fbp, fbc))
-    // });
-    // if (!response.ok) {
-    //     throw new Error(`HTTP error ${response.status}`);
-    // }
-    // const data = await response.json();
-    // return { status: response.status, message: data.message };  
-
-
-    throw new Error(" TMP sendForm is currently disabled for testing purposes", { cause: { formResponse, metaEventId, fbp, fbc } });
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/submit_form`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formResponse.toJson(metaEventId, fbp, fbc))
+    });
+    if (!response.ok) {
+        throw new Error(`HTTP error ${response.status}`);
+    }
+    const data = await response.json();
+    return { status: response.status, message: data.message };  
 }
